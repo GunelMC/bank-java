7c3ca59dae8688983deea3570148f5cc93870b54
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2022-03-06 16:49:30 +0000

    Initial commit from Spring Initializr

---
+++ src/main/java/com/example/bankjava/BankJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bankjava/BankJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
